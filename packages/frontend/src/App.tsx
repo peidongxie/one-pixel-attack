bedd4d384203bf43c2c1f9ee25f9544b5d2f8897
谢沛东 <peidongxie@outlook.com>
2021-09-10 11:09:30 +0800

    feat: 引入 recoil 进行状态管理

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import CssBaseline from '@material-ui/core/CssBaseline';
 import { ThemeProvider } from '@material-ui/core/styles';
 import type { FC } from 'react';
+import { RecoilRoot } from 'recoil';
 import MainBody from './components/main-body';
 import StickyFooter from './components/sticky-footer';
 import TopBar from './components/top-bar';
@@ -12,12 +13,14 @@ interface AppProps {
 
 const App: FC<AppProps> = () => {
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <TopBar />
-      <MainBody />
-      <StickyFooter />
-    </ThemeProvider>
+    <RecoilRoot>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <TopBar />
+        <MainBody />
+        <StickyFooter />
+      </ThemeProvider>
+    </RecoilRoot>
   );
 };
 
