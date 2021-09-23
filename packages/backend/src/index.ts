4301602d5ee919d96a6221763d4c89a5b79dba8f
谢沛东 <peidongxie@outlook.com>
2021-09-23 21:50:42 +0800

    feat: 服务框架实现跨域

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -2,26 +2,21 @@ import Server from './server';
 import type { Handler } from './server';
 
 const handler: Handler = async (req, res) => {
-  const { getBody, getHeaders, getMethod, getUrl } = req;
-  const { setBody, setHeader, setStatus, setType } = res;
-  const origin = getHeaders().origin || getUrl().host;
-  setHeader('Access-Control-Allow-Credentials', 'true');
-  setHeader('Access-Control-Allow-Headers', '*');
-  setHeader('Access-Control-Allow-Methods', '*');
-  setHeader('Access-Control-Allow-Origin', origin);
-  setHeader('Access-Control-Max-Age', '604800');
-  if (getMethod() === 'OPTIONS') {
-    return null;
-  }
+  const { getBody, getUrl } = req;
+  const { setStatus } = res;
   if (getUrl().pathname !== '/') {
     setStatus(404);
-    setType('text/plain; charset=utf-8');
-    return 'Not Found';
+    return null;
   }
   const body = await getBody();
   console.log(body);
-  setBody({ value: 'hello world' });
+  return { value: 'hello world' };
 };
 
-const server = new Server(handler);
+const server = new Server(handler, {
+  allowOrigin: (origin) => {
+    const { hostname } = new URL(origin);
+    return 'localhost' === hostname || '127.0.0.1' === hostname;
+  },
+});
 server.start(3001);
