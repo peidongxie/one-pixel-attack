4c060128af390492c46b197b8b53d9ab15cd8b66
谢沛东 <peidongxie@outlook.com>
2021-11-04 12:43:54 +0800

    feat: 扰动像素向下取整

--- packages/backend/src/adversarial-attacker/index.ts
+++ packages/backend/src/adversarial-attacker/index.ts
@@ -1,19 +1,15 @@
 import boa from '@pipcook/boa';
 import np from 'py:numpy';
-import type {
-  NumpyArray0D,
-  NumpyArray1D,
-  NumpyArray2D,
-  NumpyArray3D,
-  NumpyArray4D,
-} from 'py:numpy';
+import type { NumpyArray0D, NumpyArray1D, NumpyArray2D } from 'py:numpy';
 import optimize from 'py:scipy.optimize';
 import ImageClassifier from '../image-classifier';
 
-const { len, tuple, zip } = boa.builtins();
+const { len, tuple } = boa.builtins();
 
 class AdversarialAttacker {
   imageClassifier: ImageClassifier;
+
+  pixels?: NumpyArray2D;
 
   perturbation: number;
 
@@ -27,7 +23,7 @@ class AdversarialAttacker {
     }
   }
 
-  attack(): NumpyArray1D {
+  attack(): NumpyArray2D {
     const [row, column] = this.imageClassifier.shape;
     const bounds = new optimize.Bounds(
       tuple([0, 0, 0, 0, 0]).__mul__(this.perturbation),
@@ -45,43 +41,45 @@ class AdversarialAttacker {
         polish: false,
       }),
     );
-    return result.x;
+    const pixels = this.#getPixels(result.x);
+    this.pixels = pixels;
+    return pixels;
   }
 
-  getPrediction(x: NumpyArray1D): NumpyArray1D {
-    const { image, model, normalized } = this.imageClassifier;
-    const groups = np.expand_dims<NumpyArray2D>(x, 0);
-    const images = np.tile<NumpyArray4D>(image, [len(groups), 1, 1, 1]);
-    for (const e of zip(images, groups)) {
-      const image: NumpyArray3D = e[0];
-      const group: NumpyArray1D = e[1];
-      const pixels = np.split(group, len(group) / 5);
-      for (const pixel of pixels) {
-        const row = np.floor(pixel[0]);
-        const column = np.floor(pixel[1]);
-        const red = np.floor(pixel[2]);
-        const green = np.floor(pixel[3]);
-        const blue = np.floor(pixel[4]);
-        const colors = image.__getitem__(tuple([row, column]));
-        colors[0] = normalized ? np.divide(red, 255) : red;
-        colors[1] = normalized ? np.divide(green, 255) : green;
-        colors[2] = normalized ? np.divide(blue, 255) : blue;
-      }
-    }
-    return model.predict<NumpyArray2D>(images)[0];
+  getPrediction(): NumpyArray1D | null {
+    return this.pixels ? this.#getPrediction(this.pixels) : null;
   }
 
   #differentialEvolutionCallback = (x: NumpyArray1D): true | void => {
     const { label } = this.imageClassifier;
-    const prediction = this.getPrediction(x);
+    const prediction = this.#getPrediction(x);
     if (label !== Number(np.argmax(prediction))) return true;
   };
 
   #differentialEvolutionFunc = (x: NumpyArray1D): NumpyArray0D => {
     const { label } = this.imageClassifier;
-    const predictions = this.getPrediction(x);
+    const predictions = this.#getPrediction(x);
     return predictions[label];
   };
+
+  #getPixels(x: NumpyArray1D): NumpyArray2D {
+    const pixels = np.array<NumpyArray2D>(np.split(x, len(x) / 5));
+    return np.floor(pixels);
+  }
+
+  #getPrediction(x: NumpyArray1D | NumpyArray2D): NumpyArray1D {
+    const { image: originalImage, model, normalized } = this.imageClassifier;
+    const image = originalImage.copy();
+    const pixels = x.ndim === 1 ? this.#getPixels(x) : x;
+    for (const pixel of pixels) {
+      const [row, column, red, green, blue] = pixel;
+      const colors = image.__getitem__(tuple([row, column]));
+      colors[0] = normalized ? np.divide(red, 255) : red;
+      colors[1] = normalized ? np.divide(green, 255) : green;
+      colors[2] = normalized ? np.divide(blue, 255) : blue;
+    }
+    return model.predict<NumpyArray2D>(np.expand_dims(image, 0))[0];
+  }
 }
 
 export default AdversarialAttacker;
--- packages/backend/src/types/index.d.ts
+++ packages/backend/src/types/index.d.ts
@@ -8,6 +8,12 @@ declare global {
     step: null | number;
     stop: number;
   }
+
+  interface List<K extends number, T> extends PyObject {
+    __mul__: (n: number) => Tuple<K, T>;
+    [Symbol.iterator]: () => IterableIterator<T>;
+    [key: K]: T;
+  }
   interface Tuple<K extends number, T> extends PyObject {
     __mul__: (n: number) => Tuple<K, T>;
     [Symbol.iterator]: () => IterableIterator<T>;
