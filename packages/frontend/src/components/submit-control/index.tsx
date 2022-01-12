1932915b7a124213660def00a491bdd065d731d9
谢沛东 <peidongxie@outlook.com>
2022-01-12 10:49:57 +0800

    feat: 增加环形进度条

--- packages/frontend/src/components/submit-control/index.tsx
+++ packages/frontend/src/components/submit-control/index.tsx
@@ -1,5 +1,10 @@
-import { Button, FormGroup, makeStyles } from '@material-ui/core';
-import { type FC } from 'react';
+import {
+  Button,
+  CircularProgress,
+  FormGroup,
+  makeStyles,
+} from '@material-ui/core';
+import { useState, type FC } from 'react';
 import { useRecoilCallback, useRecoilValue } from 'recoil';
 import { formState, isValidState, resultState } from '../../utils/form';
 
@@ -14,31 +19,38 @@ const useStyles = makeStyles((theme) => ({
     flexWrap: 'nowrap',
     justifyContent: 'flex-end',
   },
+  submit: {
+    height: 38,
+    width: 88,
+  },
 }));
 
 const SubmitControl: FC<SubmitControlProps> = () => {
   const classes = useStyles();
+  const [loading, setLoading] = useState(false);
   const isVaild = useRecoilValue(isValidState);
   const handleClick = useRecoilCallback(
     ({ snapshot, set }) =>
       async () => {
         const form = await snapshot.getPromise(formState);
         if (form !== null) {
+          setLoading(true);
           const formData = new FormData();
           for (const { name, value, fileName } of form) {
             if (fileName) formData.append(name, value, fileName);
             else formData.append(name, value);
           }
-          const response = await fetch(
-            'https://run.mocky.io/v3/a92e320b-9e08-4481-9e39-ec713b92004f',
-            { body: formData, method: 'POST' },
-          );
           try {
+            const response = await fetch('http://api.peaceandlove.top:3001/', {
+              body: formData,
+              method: 'POST',
+            });
             const result = await response.json();
             set(resultState, result);
           } catch (e) {
             console.error(e);
           }
+          setLoading(false);
         }
       },
     [],
@@ -47,12 +59,13 @@ const SubmitControl: FC<SubmitControlProps> = () => {
     <FormGroup className={classes.root} row={true}>
       <Button
         aria-label={'Attack'}
+        className={classes.submit}
         color={'secondary'}
-        disabled={!isVaild}
+        disabled={!isVaild || loading}
         onClick={handleClick}
         variant={'contained'}
       >
-        {'Attack'}
+        {loading ? <CircularProgress size={20} thickness={1.8} /> : 'Attack'}
       </Button>
     </FormGroup>
   );
