5b7e35d76fbe9c964a07178957b17309223c0f3f
谢沛东 <peidongxie@outlook.com>
2022-02-06 21:02:27 +0800

    feat: 优化全局数据结构

--- packages/frontend/src/components/submit-control/index.tsx
+++ packages/frontend/src/components/submit-control/index.tsx
@@ -1,7 +1,16 @@
 import { Button, CircularProgress, FormGroup, styled } from '@material-ui/core';
-import { useState, type FC } from 'react';
-import { useRecoilCallback, useRecoilValue } from 'recoil';
-import { formState, isValidState, resultState } from '../../utils/form';
+import { type FC } from 'react';
+import {
+  useRecoilCallback,
+  useRecoilValue,
+  useRecoilValueLoadable,
+} from 'recoil';
+import {
+  formState,
+  queryIdState,
+  queryInputState,
+  queryOutputState,
+} from '../../utils/form';
 
 interface SubmitControlProps {
   [key: string]: never;
@@ -20,44 +29,31 @@ const StyledButton = styled(Button)({
 });
 
 const SubmitControl: FC<SubmitControlProps> = () => {
-  const [loading, setLoading] = useState(false);
-  const isVaild = useRecoilValue(isValidState);
+  const id = useRecoilValue(queryIdState);
+  const output = useRecoilValueLoadable(queryOutputState(id));
+  const form = useRecoilValue(formState);
   const handleClick = useRecoilCallback(
-    ({ snapshot, set }) =>
-      async () => {
-        const form = await snapshot.getPromise(formState);
-        if (form !== null) {
-          setLoading(true);
-          const formData = new FormData();
-          for (const { name, value, fileName } of form) {
-            if (fileName) formData.append(name, value, fileName);
-            else formData.append(name, value);
-          }
-          try {
-            const response = await fetch('http://localhost:3001/', {
-              body: formData,
-              method: 'POST',
-            });
-            const result = await response.json();
-            set(resultState, result);
-          } catch (e) {
-            console.error(e);
-          }
-          setLoading(false);
-        }
+    ({ set }) =>
+      () => {
+        set(queryIdState, id + 1);
+        set(queryInputState(id + 1), form);
       },
-    [],
+    [id, form],
   );
   return (
     <StyledFormGroup row={true}>
       <StyledButton
         aria-label={'Attack'}
         color={'secondary'}
-        disabled={!isVaild || loading}
+        disabled={form === null || output.state === 'loading'}
         onClick={handleClick}
         variant={'contained'}
       >
-        {loading ? <CircularProgress size={20} thickness={1.8} /> : 'Attack'}
+        {output.state === 'loading' ? (
+          <CircularProgress size={20} thickness={1.8} />
+        ) : (
+          'Attack'
+        )}
       </StyledButton>
     </StyledFormGroup>
   );
--- packages/frontend/src/utils/form.ts
+++ packages/frontend/src/utils/form.ts
@@ -1,4 +1,4 @@
-import { atom, selector } from 'recoil';
+import { atom, atomFamily, selector, selectorFamily } from 'recoil';
 
 interface FormItem {
   name: string;
@@ -46,6 +46,15 @@ const imageIsNormalizedState = atom({
   default: true,
 });
 
+const imageIsNumpyState = selector({
+  key: 'imageIsNumpyState',
+  get: ({ get }) => {
+    const file = get(imageFileState);
+    if (!file) return false;
+    return file.name.endsWith('.npy');
+  },
+});
+
 // label control
 
 const labelIsDefaultState = atom({
@@ -72,236 +81,256 @@ const perturbationPixelState = atom({
 
 // form field
 
-const modelState = selector<FormItem | null>({
-  key: 'modelState',
-  get: ({ get }) => {
-    const modelIsDefault = get(modelIsDefaultState);
-    if (modelIsDefault) {
-      return {
-        name: 'model',
-        value: 'default',
-      };
-    }
-    const modelFile = get(modelFileState);
-    if (!modelFile) return null;
-    const modelIsNormalized = get(modelIsNormalizedState);
-    return {
-      name: 'model',
-      value: modelFile,
-      fileName: modelIsNormalized ? 'normalized.h5' : 'raw.h5',
-    };
-  },
+const formFieldState = selectorFamily<
+  FormItem | null,
+  'model' | 'image' | 'label' | 'perturbation'
+>({
+  key: 'formFieldState',
+  get:
+    (field) =>
+    ({ get }) => {
+      switch (field) {
+        case 'model': {
+          const modelIsDefault = get(modelIsDefaultState);
+          if (modelIsDefault) {
+            return {
+              name: 'model',
+              value: 'default',
+            };
+          }
+          const modelFile = get(modelFileState);
+          if (!modelFile) return null;
+          const modelIsNormalized = get(modelIsNormalizedState);
+          return {
+            name: 'model',
+            value: modelFile,
+            fileName: modelIsNormalized ? 'normalized.h5' : 'raw.h5',
+          };
+        }
+        case 'image': {
+          const imageIsDefault = get(imageIsDefaultState);
+          if (imageIsDefault) {
+            return {
+              name: 'image',
+              value: 'default',
+            };
+          }
+          const imageFile = get(imageFileState);
+          if (!imageFile) return null;
+          const imageIsNumpy = get(imageIsNumpyState);
+          if (!imageIsNumpy) {
+            return {
+              name: 'image',
+              value: imageFile,
+              fileName: 'raw.png',
+            };
+          }
+          const imageIsNormalized = get(imageIsNormalizedState);
+          return {
+            name: 'image',
+            value: imageFile,
+            fileName: imageIsNormalized ? 'normalized.npy' : 'raw.npy',
+          };
+        }
+        case 'label': {
+          const labelIsDefault = get(labelIsDefaultState);
+          if (labelIsDefault) {
+            return {
+              name: 'label',
+              value: 'default',
+            };
+          }
+          const labelIndex = get(labelIndexState);
+          if (isNaN(labelIndex)) return null;
+          return {
+            name: 'label',
+            value: String(labelIndex),
+          };
+        }
+        case 'perturbation': {
+          const perturbationIsDefault = get(perturbationIsDefaultState);
+          if (perturbationIsDefault) {
+            return {
+              name: 'perturbation',
+              value: 'default',
+            };
+          }
+          const perturbationPixel = get(perturbationPixelState);
+          if (isNaN(perturbationPixel)) return null;
+          return {
+            name: 'perturbation',
+            value: String(perturbationPixel),
+          };
+        }
+        default:
+          return null;
+      }
+    },
 });
-
-const imageIsNumpyState = selector({
-  key: 'imageIsNumpyState',
-  get: ({ get }) => {
-    const file = get(imageFileState);
-    if (!file) return false;
-    return file.name.endsWith('.npy');
-  },
-});
-
-const imageState = selector<FormItem | null>({
-  key: 'imageState',
-  get: ({ get }) => {
-    const imageIsDefault = get(imageIsDefaultState);
-    if (imageIsDefault) {
-      return {
-        name: 'image',
-        value: 'default',
-      };
-    }
-    const imageFile = get(imageFileState);
-    if (!imageFile) return null;
-    const imageIsNumpy = get(imageIsNumpyState);
-    if (!imageIsNumpy) {
-      return {
-        name: 'image',
-        value: imageFile,
-        fileName: 'raw.png',
-      };
-    }
-    const imageIsNormalized = get(imageIsNormalizedState);
-    return {
-      name: 'image',
-      value: imageFile,
-      fileName: imageIsNormalized ? 'normalized.npy' : 'raw.npy',
-    };
-  },
-});
-
-const labelState = selector<FormItem | null>({
-  key: 'labelState',
-  get: ({ get }) => {
-    const labelIsDefault = get(labelIsDefaultState);
-    if (labelIsDefault) {
-      return {
-        name: 'label',
-        value: 'default',
-      };
-    }
-    const labelIndex = get(labelIndexState);
-    if (isNaN(labelIndex)) return null;
-    return {
-      name: 'label',
-      value: String(labelIndex),
-    };
-  },
-});
-
-const perturbationState = selector<FormItem | null>({
-  key: 'perturbationState',
-  get: ({ get }) => {
-    const perturbationIsDefault = get(perturbationIsDefaultState);
-    if (perturbationIsDefault) {
-      return {
-        name: 'perturbation',
-        value: 'default',
-      };
-    }
-    const perturbationPixel = get(perturbationPixelState);
-    if (isNaN(perturbationPixel)) return null;
-    return {
-      name: 'perturbation',
-      value: String(perturbationPixel),
-    };
-  },
-});
-
-// form state
 
 const formState = selector({
   key: 'formState',
   get: ({ get }) => {
-    const model = get(modelState);
-    if (model === null) return null;
-    const image = get(imageState);
-    if (image === null) return null;
-    const label = get(labelState);
-    if (label === null) return null;
-    const perturbation = get(perturbationState);
-    if (perturbation === null) return null;
-    return [image, model, label, perturbation];
+    const formData = new FormData();
+    for (const field of ['model', 'image', 'label', 'perturbation'] as const) {
+      const item = get(formFieldState(field));
+      if (item === null) return null;
+      const { name, value, fileName } = item;
+      if (fileName) formData.append(name, value, fileName);
+      else formData.append(name, value);
+    }
+    return formData;
   },
 });
 
-const isValidState = selector({
-  key: 'isValidState',
-  get: ({ get }) => {
-    const form = get(formState);
-    return form !== null;
-  },
+// query
+
+const queryIdState = atom({
+  key: 'queryIdState',
+  default: -1,
+});
+
+const queryInputState = atomFamily<FormData | null, number>({
+  key: 'queryInputState',
+  default: null,
+});
+
+const queryOutputState = selectorFamily<Result | null, number>({
+  key: 'queryOutputState',
+  get:
+    (id) =>
+    async ({ get }) => {
+      const input = get(queryInputState(id));
+      if (input === null) return null;
+      const response = await fetch('http://localhost:3001', {
+        body: input,
+        method: 'POST',
+      });
+      return response.json();
+    },
 });
 
 // result data
 
-const resultState = atom<Result | null>({
-  key: 'resultState',
-  default: null,
-});
-
-const shapeState = selector<[number, number, number, number, number]>({
+const shapeState = selectorFamily<
+  [number, number, number, number, number],
+  number
+>({
   key: 'shapeState',
-  get: ({ get }) => {
-    const result = get(resultState);
-    if (!result?.image) return [0, 0, 0, 0, 4];
-    const rowFamily = result.image.length;
-    const row = Math.ceil(200 / rowFamily);
-    const columnFamily = result.image[0].length;
-    const column = Math.ceil(200 / columnFamily);
-    return [rowFamily, row, columnFamily, column, 4];
-  },
+  get:
+    (id) =>
+    ({ get }) => {
+      const output = get(queryOutputState(id));
+      if (!output?.image) return [0, 0, 0, 0, 4];
+      const rowFamily = output.image.length;
+      const row = Math.ceil(200 / rowFamily);
+      const columnFamily = output.image[0].length;
+      const column = Math.ceil(200 / columnFamily);
+      return [rowFamily, row, columnFamily, column, 4];
+    },
 });
 
-const bufferState = selector({
+const bufferState = selectorFamily<
+  [Uint8ClampedArray, Uint8ClampedArray],
+  number
+>({
   key: 'BufferState',
-  get: ({ get }) => {
-    const result = get(resultState);
-    const [s0, s1, s2, s3, s4] = get(shapeState);
-    const length = s0 * s1 * s2 * s3 * s4;
-    const bufferBefore = new Uint8ClampedArray(length);
-    if (result?.image) {
-      for (let i0 = 0; i0 < s0; i0++) {
-        for (let i1 = 0; i1 < s1; i1++) {
-          for (let i2 = 0; i2 < s2; i2++) {
-            for (let i3 = 0; i3 < s3; i3++) {
-              const offset = (((i0 * s1 + i1) * s2 + i2) * s3 + i3) * s4;
-              const [red, green, blue, alpha = 100] = result.image[i0][i2];
-              bufferBefore[offset] = red;
-              bufferBefore[offset + 1] = green;
-              bufferBefore[offset + 2] = blue;
-              bufferBefore[offset + 3] = alpha;
+  get:
+    (id) =>
+    ({ get }) => {
+      const output = get(queryOutputState(id));
+      const [s0, s1, s2, s3, s4] = get(shapeState(id));
+      const length = s0 * s1 * s2 * s3 * s4;
+      const bufferBefore = new Uint8ClampedArray(length);
+      if (output?.image) {
+        for (let i0 = 0; i0 < s0; i0++) {
+          for (let i1 = 0; i1 < s1; i1++) {
+            for (let i2 = 0; i2 < s2; i2++) {
+              for (let i3 = 0; i3 < s3; i3++) {
+                const offset = (((i0 * s1 + i1) * s2 + i2) * s3 + i3) * s4;
+                const [red, green, blue, alpha = 100] = output.image[i0][i2];
+                bufferBefore[offset] = red;
+                bufferBefore[offset + 1] = green;
+                bufferBefore[offset + 2] = blue;
+                bufferBefore[offset + 3] = alpha;
+              }
             }
           }
         }
       }
-    }
-    const bufferAfter = new Uint8ClampedArray(bufferBefore);
-    if (result?.pixels) {
-      for (const [i0, i2, red, green, blue, alpha = 100] of result.pixels) {
-        for (let i1 = 0; i1 < s1; i1++) {
-          for (let i3 = 0; i3 < s3; i3++) {
-            const offset = (((i0 * s1 + i1) * s2 + i2) * s3 + i3) * s4;
-            bufferAfter[offset] = red;
-            bufferAfter[offset + 1] = green;
-            bufferAfter[offset + 2] = blue;
-            bufferAfter[offset + 3] = alpha;
+      const bufferAfter = new Uint8ClampedArray(bufferBefore);
+      if (output?.pixels) {
+        for (const [i0, i2, red, green, blue, alpha = 100] of output.pixels) {
+          for (let i1 = 0; i1 < s1; i1++) {
+            for (let i3 = 0; i3 < s3; i3++) {
+              const offset = (((i0 * s1 + i1) * s2 + i2) * s3 + i3) * s4;
+              bufferAfter[offset] = red;
+              bufferAfter[offset + 1] = green;
+              bufferAfter[offset + 2] = blue;
+              bufferAfter[offset + 3] = alpha;
+            }
           }
         }
       }
-    }
-    return [bufferBefore, bufferAfter];
-  },
+      return [bufferBefore, bufferAfter];
+    },
 });
 
-const imageBeforeState = selector({
-  key: 'imageBeforeState',
-  get: ({ get }) => {
-    const [s0, s1, s2, s3, s4] = get(shapeState);
-    const buffer = get(bufferState);
-    if (s0 * s1 * s2 * s3 * s4) {
-      return new ImageData(buffer[0], s2 * s3, s0 * s1);
-    } else {
-      return null;
-    }
-  },
+const imagesState = selectorFamily<
+  [ImageData, ImageData] | [null, null],
+  number
+>({
+  key: 'imagesState',
+  get:
+    (id) =>
+    ({ get }) => {
+      const [s0, s1, s2, s3, s4] = get(shapeState(id));
+      const buffer = get(bufferState(id));
+      if (s0 * s1 * s2 * s3 * s4) {
+        return [
+          new ImageData(buffer[0], s2 * s3, s0 * s1),
+          new ImageData(buffer[1], s2 * s3, s0 * s1),
+        ];
+      } else {
+        return [null, null];
+      }
+    },
 });
 
-const imageAfterState = selector({
-  key: 'imageAfterState',
-  get: ({ get }) => {
-    const [s0, s1, s2, s3, s4] = get(shapeState);
-    const buffer = get(bufferState);
-    if (s0 * s1 * s2 * s3 * s4) {
-      return new ImageData(buffer[1], s2 * s3, s0 * s1);
-    } else {
-      return null;
-    }
+const predictionsState = selectorFamily<[number[], number[], string[]], number>(
+  {
+    key: 'predictionsState',
+    get:
+      (id) =>
+      ({ get }) => {
+        const output = get(queryOutputState(id));
+        if (!output?.predictions) return [[], [], []];
+        const [predictionBefore, predictionAfter] = output.predictions;
+        const label = predictionBefore.map((value, index) => 'C' + index);
+        return [predictionBefore, predictionAfter, label];
+      },
   },
-});
+);
 
 export {
   bufferState,
   formState,
-  imageAfterState,
-  imageBeforeState,
+  formFieldState,
   imageFileState,
   imageIsDefaultState,
   imageIsNormalizedState,
   imageIsNumpyState,
-  imageState,
-  isValidState,
+  imagesState,
   labelIndexState,
   labelIsDefaultState,
-  labelState,
   modelFileState,
   modelIsDefaultState,
   modelIsNormalizedState,
-  modelState,
   perturbationIsDefaultState,
   perturbationPixelState,
-  perturbationState,
-  resultState,
+  predictionsState,
+  queryIdState,
+  queryInputState,
+  queryOutputState,
   shapeState,
 };
