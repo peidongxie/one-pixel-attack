6a7533210a9bbeaafc5804569f9d1dc260d97980
谢沛东 <peidongxie@outlook.com>
2023-06-19 12:34:56 +0800

    feat: 实现单独模型类

---
+++ packages/backend/src/model.py
@@ -0,0 +1,32 @@
+import numpy as np
+import tensorflow as tf
+
+
+class Model:
+    def __init__(self, data: tf.keras.models.Sequential | str) -> None:
+        if type(data) is not str:
+            self._data = data
+        else:
+            self._data = tf.keras.models.load_model(
+                filepath=data,
+            )
+        shape = self._data.layers[0].input_shape
+        if len(shape) is 2:
+            self._row = shape[0]
+            self._column = shape[1]
+            self._channel = 1
+            self._data = tf.keras.models.Sequential(
+                layers=[
+                    tf.keras.layers.Reshape(
+                        target_shape=shape,
+                        input_shape=(self._row, self._column, 1),
+                    ),
+                    self._data,
+                ],
+            )
+        elif len(shape) is 3:
+            self._row = shape[0]
+            self._column = shape[1]
+            self._channel = shape[2]
+        else:
+            raise ValueError("Bad model")
