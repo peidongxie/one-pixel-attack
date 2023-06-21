488cb9fa9d777fcca6c82231dc195f4cc5627cc4
谢沛东 <peidongxie@outlook.com>
2023-06-21 12:34:56 +0800

    feat: 为模型追加缩放层

--- packages/backend/src/model.py
+++ packages/backend/src/model.py
@@ -4,12 +4,25 @@ import tensorflow as tf
 
 class Model:
     def __init__(self, data: tf.keras.models.Sequential | str) -> None:
+        # data source
         if type(data) is not str:
             self._data = data
         else:
             self._data = tf.keras.models.load_model(
                 filepath=data,
             )
+        # data value
+        if not isinstance(self._data.layers[0], tf.keras.layers.Rescaling):
+            self._data = tf.keras.models.Sequential(
+                layers=[
+                    tf.keras.layers.Rescaling(
+                        scale=1./255,
+                        input_shape=self._data.layers[0].input_shape,
+                    ),
+                    self._data,
+                ],
+            )
+        # data shape
         shape = self._data.layers[0].input_shape
         if len(shape) is 2:
             self._row = shape[0]
